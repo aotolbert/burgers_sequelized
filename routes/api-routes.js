f6c41d96063eb34d55f19323a825c25f0aa2d5f9
aotolbert@gmail.com <aotolbert@gmail.com>
2018-10-12 17:57:29 -0400

    adding but not devouring

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,4 +12,14 @@ module.exports = (app) => {
             name: req.body.name
         }).then(newBurger => res.json(newBurger));
     });
+
+    app.put("/api/devour/:id", (req, res) => {
+        db.Burger.update(
+            isEaten = true, {
+                where: {
+                    id: req.params.id
+                }
+            }
+        ).then(dbBurgers => res.json(dbBurgers));
+    })
 }
